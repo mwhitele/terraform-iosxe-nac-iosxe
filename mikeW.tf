51c9f733d8d4e16ae2be5c171dc4f0941e73eb83
Stanchan <stanchan@cisco.com>
2025-08-12 13:08:09 -0500

    test

--- mikeW.tf
+++ mikeW.tf
@@ -1,1 +1,9 @@
-Hello World
+module "model" {
+  source = "./modules/model"
+
+  yaml_directories          = var.yaml_directories
+  yaml_files                = var.yaml_files
+  model                     = var.model
+  write_model_file          = var.write_model_file
+  write_default_values_file = var.write_default_values_file
+}
